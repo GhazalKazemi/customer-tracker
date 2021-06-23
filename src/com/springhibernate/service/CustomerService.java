0b03855d25b16d6758f0c34c32c82d41333093a3
Ghazal <ghazal.kazemi@gmail.com>
2021-06-23 16:41:33 +1000

    Create customer service

---
+++ src/com/springhibernate/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.springhibernate.service;
+
+import com.springhibernate.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getCustomers();
+}
