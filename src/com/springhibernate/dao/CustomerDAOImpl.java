49490330e088ddb0eb81851b3b304682bd82958f
Ghazal <ghazal.kazemi@gmail.com>
2021-06-24 11:15:46 +1000

    Sort table entries by last name

--- src/com/springhibernate/dao/CustomerDAOImpl.java
+++ src/com/springhibernate/dao/CustomerDAOImpl.java
@@ -18,7 +18,7 @@ public class CustomerDAOImpl implements CustomerDAO{
     public List<Customer> getCustomers() {
         Session session = sessionFactory.getCurrentSession();
 
-        Query<Customer> query = session.createQuery("from Customer", Customer.class);
+        Query<Customer> query = session.createQuery("from Customer order by lastName", Customer.class);
 
         List<Customer> customers = query.getResultList();
         return customers;
