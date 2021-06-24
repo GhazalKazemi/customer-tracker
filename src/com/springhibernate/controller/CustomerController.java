906c8105faec25cf187f91e06d0aef15957f5212
Ghazal <ghazal.kazemi@gmail.com>
2021-06-24 11:32:57 +1000

    Pre populate customer form by getting customer based on the id

--- src/com/springhibernate/controller/CustomerController.java
+++ src/com/springhibernate/controller/CustomerController.java
@@ -6,10 +6,7 @@ import com.springhibernate.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,5 +33,13 @@ public class CustomerController {
     public String saveCustomer(@ModelAttribute("customer") Customer customer){
         customerService.saveCustomer(customer);
         return "redirect:/customer/list";
+    }
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int customerId,
+                                    Model model){
+        Customer customer = customerService.getCustomer(customerId);
+        model.addAttribute("customer", customer);
+        return "customer-form";
+
     }
 }
