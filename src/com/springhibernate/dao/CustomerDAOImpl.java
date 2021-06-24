594ca43b9761ace23dd5d0b082e4b31a579af0fe
Ghazal <ghazal.kazemi@gmail.com>
2021-06-24 11:02:49 +1000

    Use the current session to save the new customer

--- src/com/springhibernate/dao/CustomerDAOImpl.java
+++ src/com/springhibernate/dao/CustomerDAOImpl.java
@@ -23,4 +23,10 @@ public class CustomerDAOImpl implements CustomerDAO{
         List<Customer> customers = query.getResultList();
         return customers;
     }
+
+    @Override
+    public void saveCustomer(Customer customer) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(customer);
+    }
 }
