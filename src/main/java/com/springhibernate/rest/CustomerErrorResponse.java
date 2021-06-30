35c4c13fe2ec3cc64c87a71a6c01d7aa7dd1b440
Ghazal <ghazal.kazemi@gmail.com>
2021-06-30 17:18:06 +1000

    Create exception handler for controller

--- src/main/java/com/springhibernate/rest/CustomerErrorResponse.java
+++ src/main/java/com/springhibernate/rest/CustomerErrorResponse.java
@@ -6,7 +6,13 @@ public class CustomerErrorResponse {
     private String message;
     private long timeStamp;
 
-    CustomerErrorResponse(){}
+    public CustomerErrorResponse(){}
+
+    public CustomerErrorResponse(int status, String message, long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
 
     public int getStatus() {
         return status;
---
+++ src/main/java/com/springhibernate/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.springhibernate.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException e){
+        CustomerErrorResponse response = new CustomerErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
