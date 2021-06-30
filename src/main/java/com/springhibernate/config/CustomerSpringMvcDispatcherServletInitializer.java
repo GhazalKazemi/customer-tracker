5ff14869116459496c02f386901f6b459b0e1611
Ghazal <ghazal.kazemi@gmail.com>
2021-06-30 12:23:26 +1000

    Set up dispatcher servlet insitializer

--- src/main/java/com/springhibernate/config/CustomerSpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/springhibernate/config/CustomerSpringMvcDispatcherServletInitializer.java
@@ -1,2 +1,20 @@
-package com.springhibernate.config;public class CustomerSpringMvcDispatcherServletInitializer {
+package com.springhibernate.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class CustomerSpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {CustomerAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
