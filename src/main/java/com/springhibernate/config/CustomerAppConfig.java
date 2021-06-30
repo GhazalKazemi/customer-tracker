7853bf70aae4310f647fe03b9b653e748fc96d9a
Ghazal <ghazal.kazemi@gmail.com>
2021-06-30 13:13:41 +1000

    Set up session factory and transaction manager

--- src/main/java/com/springhibernate/config/CustomerAppConfig.java
+++ src/main/java/com/springhibernate/config/CustomerAppConfig.java
@@ -1,12 +1,15 @@
 package com.springhibernate.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -70,5 +73,30 @@ public class CustomerAppConfig {
         String propertyValue = environment.getProperty(propertyName);
         int intPropertyValue = Integer.parseInt(propertyValue);
         return intPropertyValue;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+
+        // create session factorys
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        // set the properties
+        sessionFactory.setDataSource(secureDataSource());
+        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        // setup transaction manager based on session factory
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
     }
 }
