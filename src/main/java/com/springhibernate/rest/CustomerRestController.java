f6ce968779c5896db71aff14c0d47a05aafba974
Ghazal <ghazal.kazemi@gmail.com>
2021-06-30 16:44:29 +1000

    Create rest controller to get a single customer

--- src/main/java/com/springhibernate/rest/CustomerRestController.java
+++ src/main/java/com/springhibernate/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.springhibernate.entity.Customer;
 import com.springhibernate.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,9 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
+    }
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+        return customerService.getCustomer(customerId);
     }
 }
