45148df50847d7c50ffccdd5407477c670f0abc5
Ghazal <ghazal.kazemi@gmail.com>
2021-06-30 16:09:42 +1000

    Create rest controller to get all customers

--- src/main/java/com/springhibernate/rest/CustomerRestController.java
+++ src/main/java/com/springhibernate/rest/CustomerRestController.java
@@ -1,2 +1,22 @@
-package com.springhibernate.rest;public class CustomerRestController {
+package com.springhibernate.rest;
+
+import com.springhibernate.entity.Customer;
+import com.springhibernate.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+        return customerService.getCustomers();
+    }
 }
