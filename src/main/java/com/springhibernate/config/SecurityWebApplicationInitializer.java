6b4ac8412103d8166f8daac5879e68d7e0a13c4e
Ghazal <ghazal.kazemi@gmail.com>
2021-07-03 16:22:40 +1000

    Enable Spring security for web application

---
+++ src/main/java/com/springhibernate/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.springhibernate.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
