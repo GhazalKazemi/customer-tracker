5033be081d586bd122f90373ad7b69a80d22a083
Ghazal <ghazal.kazemi@gmail.com>
2021-07-03 17:17:27 +1000

    Create endpoint specific suthorization

--- src/main/java/com/springhibernate/config/DemoSecurityConfig.java
+++ src/main/java/com/springhibernate/config/DemoSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.springhibernate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +24,13 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/customers/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/customers").hasRole("EMPLOYEE")
+                .antMatchers(HttpMethod.GET, "/api/customers/**").hasRole("EMPLOYEE")
+                .antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/customers/**").hasRole("ADMIN")
                 .and()
                 .httpBasic()
                 .and()
